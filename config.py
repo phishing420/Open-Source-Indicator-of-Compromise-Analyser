3f5c8894f3bb8babac83afded95f77db5c5c1c72
phishing420 <boopathi.mscs1232543@nfsu.ac.in>
2025-04-27 19:57:13 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#config.py
+API_KEY = ""
+URLSCAN_API_KEY = ""
+ABUSE_CH_API_KEY = ""
+ABUSEIPDB_API_KEY = ""
+
+
+#URL
+API_URL_Ip = "https://www.virustotal.com/api/v3/ip_addresses/"
+API_URL_Domain = "https://www.virustotal.com/api/v3/domains/"
